db343c0484a0fc7beecf58966466b864fdc2d425
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-14 00:02:28 +0900

    googleログイン機能の実装 ローカルストレージの利用

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./components/Home";
 import { CreatePost } from "./components/CreatePost";
@@ -7,13 +8,15 @@ import { Logout } from "./components/Logout";
 import { Navbar } from "./components/Navbar";
 
 function App() {
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,27 @@
-export const Login = () => {
-  return <div>Login</div>;
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  setIsAuth: (isAuth: boolean) => void;
+};
+
+export const Login = ({ setIsAuth }: Props) => {
+  const navigate = useNavigate();
+  const loginWithGoogle = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      // console.log(result);
+      localStorage.setItem("isAuth", "true");
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
