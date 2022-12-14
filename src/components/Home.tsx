b3f640c534b41b255e5c5f57c5254fc76e07638e
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-14 16:38:32 +0900

    firestoreからデータを取得 ホームで表示

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,44 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import "./styles/Home.css";
 
+type Doc = {
+  author?: { username: string; id: string };
+  id?: string;
+  postText?: string;
+  title?: string;
+};
+
 export const Home = () => {
+  const [postList, setPostList] = useState<Doc[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      // type Docのプロパティに?をつけないと下記がerrorに。typescriptムズイ。
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          今はお腹があまり空いていません。
-          何を食べようかな。昨日の肉まんおいしかったなぁ親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@はたの</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author?.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
